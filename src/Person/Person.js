9983bcdd3cdf69b5d787c8bb74a8c7ba90c00b0e
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2019-08-12 23:44:54 +0530

    added-eject-for-webpack-config-to-import-css-modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css';
 //import Radium from 'radium';
 
 const person = (props) => {
@@ -11,7 +11,7 @@ const person = (props) => {
     //     }
     // }
     return (
-        <div className="Person">
+        <div className={classes.Person}>
              <p onClick={props.click}>I am {props.name}, I am {props.age} years old</p>
              <p>{props.children}</p>
              <input  type="text" onChange={props.changed} value={props.name}/>
